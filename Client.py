2768a798fff640af2d3bb56045814a269be3822c
Ravija Maheshwari <iamravija@yahoo.com>
2019-11-07 04:34:49 +1100

    Cleaned up client

--- Client.py
+++ Client.py
@@ -10,32 +10,7 @@ def client_program():
     client_socket.connect((host, port))  # connect to the server
 
     print("Client starting...")
-    
-    def login(client_socket):
-        isReceived = "Password incorrect"
-        uname = raw_input('Username:')
-        i = 0
-        
-        while (i < 3 and isReceived == "Password incorrect"):
-            client_socket.send(uname)
-            password = raw_input('Password:')
-            client_socket.send(password)
-            isReceived = client_socket.recv(1024)
 
-            if isReceived == "Credentials authenticated":
-                print("Welcome to the greatest messaging application ever!")
-                return True
-            elif isReceived == "Password incorrect":
-                print ("Invalid Password. Please try again")
-            i = i + 1
-        print(isReceived)
-        return False
-
-    # while isLogged == False:
-    #     isLogged = login(client_socket)
-    
-    # if(isLogged == True):
-    #     print ("Logged in")
     while 1:
         data = client_socket.recv(1024).decode()
         print (data),
